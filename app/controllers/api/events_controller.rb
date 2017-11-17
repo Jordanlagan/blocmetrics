bc50528b04f57664643a7e67cafa02eddf55f874
Jordan Lagan <jordanlagan000@gmail.com>
2017-11-17 17:35:52 -0500

    Can now use API to post events to BlocMetrics

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,6 +2,7 @@ class API::EventsController < ApplicationController
 
   skip_before_action :verify_authenticity_token
 
+=begin
   before_action :set_access_control_headers
 
   def set_access_control_headers
@@ -13,20 +14,23 @@ class API::EventsController < ApplicationController
     headers['Access-Control-Allow-Headers'] = 'Content-Type'
 
   end
+=end
 
   def create
-    @registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
-    unless @registered_application.present?
+    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    if registered_application
+      event = registered_application.events.build(event_params)
+      
+      if event.save
+        render json: event, status: :created
+      else
+        render json: {errors: event.errors}, status: :unprocessable_entity
+      end
+    else
       render json: "Unregistered Application", status: :unprocessable_entity
     end
 
-    @event = @registered_application.events.new(event_params)
 
-    if @event.save
-      render json: @event, status: :created
-    else
-      render json: {erros: @event.errors}, status: :unprocessable_entity
-    end
   end
 
   def preflight
